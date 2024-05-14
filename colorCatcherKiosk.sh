924190b204273ab7bba00350b7ee8532c0e77a0a
Pierre Rossel <>
2024-05-14 17:30:34 +0200

    fix

--- colorCatcherKiosk.sh
+++ colorCatcherKiosk.sh
@@ -4,4 +4,7 @@
 cd "$(dirname "$0")"
 
 # Run an http server and launch chrome in kiosk mode
-python3 -m http.server 5500 & /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --chrome-frame --autoplay-policy=no-user-gesture-required --kiosk "http://localhost:5500" & 
+python3 -m http.server 5500 & 
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --chrome-frame --autoplay-policy=no-user-gesture-required --kiosk "http://localhost:5500"
+
+echo done
