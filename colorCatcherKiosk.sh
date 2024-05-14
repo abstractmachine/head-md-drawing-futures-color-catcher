89ab31ab3f9d662868783e4612dba3bf41fa3783
Pierre Rossel <>
2024-05-14 16:13:56 +0200

    kiosk launch script

---
+++ colorCatcherKiosk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Run an http server and launch chrome in kiosk mode
+python3 -m http.server 5500 & /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --chrome-frame --autoplay-policy=no-user-gesture-required --kiosk "http://localhost:5500" & 
